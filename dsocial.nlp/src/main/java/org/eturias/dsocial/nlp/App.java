0547270a05503224e4cc1cc8109e87c025bbd24f
Vicente García Díaz <vicegd@gmail.com>
2016-11-28 18:33:33 +0100

    Starting...

---
+++ dsocial.nlp/src/main/java/org/eturias/dsocial/nlp/App.java
@@ -0,0 +1,13 @@
+package org.eturias.dsocial.nlp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
